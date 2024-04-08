547a12df0e2bfaec97df6fa6f18faad9b1f62c5b
Aadarsh Dangi <78791785+LegXgodGaming@users.noreply.github.com>
2024-04-09 01:08:23 +0530

    utils done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-n
+import dotenv from "dotenv"
+
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+.then( ()=>{
+      app.on ("error",(error) =>{
+        console.log("Error",error);
+        throw error
+      })
+      app.listen( process.env.PORT || 8000, () =>{
+        console.log(`Server is running at port : ${
+            process.env.PORT
+        }`);
+      }
+
+      )
+
+}) 
+.catch( (error)=>{
+    console.log(`MongoDB connection error" ,error`);
+})
